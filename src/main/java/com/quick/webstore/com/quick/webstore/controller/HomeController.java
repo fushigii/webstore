fe30c91912a51c76ab21a93a249d71425883b9f3
esteban Lopez <esteba_epam@epam.com>
2018-02-28 19:22:41 -0600

    first commit

---
+++ src/main/java/com/quick/webstore/com/quick/webstore/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.quick.webstore.com.quick.webstore.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String welcome(Model model) {
+
+        model.addAttribute("gretting", "Welcome to web Store!");
+        model.addAttribute("tagline","The one and only amazing web store");
+        return "welcome";
+
+    }
+}
